5ad453bf20b0a4eca968d06d5f562dd38eea4346
fchrgrib <fahrian.alifudin@gmail.com>
2023-01-27 14:24:56 +0700

    progress to collect wallpaper

--- main.go
+++ main.go
@@ -1,30 +1,23 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"walpapperCollectRestAPI/controller"
-	"walpapperCollectRestAPI/lib/middleware"
-)
-
 func main() {
-	routs := gin.Default()
-
-	routs.POST("/register", controller.CreateUserAuth)
-	routs.POST("/login", controller.LoginController)
-	//routs.GET("/token", func(c *gin.Context) {
+	//routs := gin.Default()
+	//
+	//routs.POST("/register", controller.CreateUserAuth)
+	//routs.POST("/login", controller.LoginController)
+	////routs.GET("/token", func(c *gin.Context) {
+	////	c.JSON(http.StatusOK, gin.H{
+	////		"token": c.GetHeader("Cookie"),
+	////	})
+	////})
+	//
+	//privateRouts := routs.Group("/private")
+	//privateRouts.Use(middleware.JWT)
+	//privateRouts.GET("/user", func(c *gin.Context) {
 	//	c.JSON(http.StatusOK, gin.H{
-	//		"token": c.GetHeader("Cookie"),
+	//		"status": "ini private lohhh",
 	//	})
 	//})
-
-	privateRouts := routs.Group("/private")
-	privateRouts.Use(middleware.JWT)
-	privateRouts.GET("/user", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "ini private lohhh",
-		})
-	})
-
-	routs.Run()
+	//
+	//routs.Run()
 }
