8d6250154afd344fd12b1dd75cb21f47885bd4a4
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-05-21 14:41:55 +0700

    revision WallpaperCollection.go

--- controller/WallpaperCollection.go
+++ controller/WallpaperCollection.go
@@ -1,12 +1,25 @@
 package controller
 
 import (
+	"github.com/database"
+	"github.com/database/models"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/tools"
 	"net/http"
 )
 
 func WallpaperCollection(c *gin.Context) {
+
+	var wallpaperCollect []models.WallpaperCollection
+
+	db, err := database.ConnectDB()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": err.Error(),
+		})
+		return
+	}
 
 	id, err := tools.GetUserIdFromCookies(c)
 	if err != nil {
@@ -16,15 +29,22 @@ func WallpaperCollection(c *gin.Context) {
 		return
 	}
 
-	dir1 := http.Dir("././assets/" + id + "/wallpaper_collection")
-	dir2 := http.Dir("././assets/" + id + "/profile")
 	UserData, err := tools.GetUserDataWithId(id)
-	c.FileFromFS("", dir2)
 	c.JSON(http.StatusOK, gin.H{
 		"user_name":    UserData.UserName,
 		"phone_number": UserData.PhoneNumber,
 		"email":        UserData.Email,
 	})
 
-	c.FileFromFS("", dir1)
+	if err := db.Table("wallpaper_collect").Where("user_id = ?", id).Find(&wallpaperCollect).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": err.Error(),
+		})
+		return
+	}
+
+	for _, value := range wallpaperCollect {
+		c.File(value.Path + value.ImageName)
+	}
+	return
 }
