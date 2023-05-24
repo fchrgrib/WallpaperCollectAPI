3d8982748087f8384cc1f44c797b6940c3aee1ea
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-05-25 05:31:51 +0700

    add static photo profile

--- handlers/routers/Images.go
+++ handlers/routers/Images.go
@@ -10,6 +10,7 @@ import (
 
 func Images(routers *gin.Engine) {
 	var wallpaper []models.WallpaperCollectionDB
+	var photoProfile []models.UserPhotoProfileDB
 
 	db, err := database.ConnectDB()
 	if err != nil {
@@ -17,6 +18,7 @@ func Images(routers *gin.Engine) {
 	}
 
 	db.Table("wallpaper_collect").Find(&wallpaper)
+	db.Table("photo_profile").Find(&photoProfile)
 
 	rImage := routers.Group("/images")
 	rImage.Use(middleware.AuthWithToken)
@@ -26,6 +28,16 @@ func Images(routers *gin.Engine) {
 			rImage.Static(values.ImageId, values.Path)
 		}
 	}
-
 	rImage.DELETE("/:id/delete", wallpaperpage.DeleteWallpaperController)
+
+	rProfile := routers.Group("/photo_profile")
+
+	if len(photoProfile) != 0 {
+		for _, value := range photoProfile {
+			if value.Path != "" {
+				rProfile.Static(value.UserId, value.Path)
+			}
+		}
+	}
+
 }
