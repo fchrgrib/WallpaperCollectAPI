500121f52ab6e64d1d024a6c418a1d0d211cd1cd
fchrgrib <16521492@mahasiswa.itb.ac.id>
2023-05-31 03:03:04 +0700

    experiment with google sigin

--- models/JSONModels.go
+++ models/JSONModels.go
@@ -8,8 +8,8 @@ type UserLogin struct {
 }
 
 type User struct {
-	Id           string `json:"user_id"`
-	UserName     string `json:"user_name"`
+	Id           string `json:"user_id" gorm:"primaryKey"`
+	UserName     string `json:"user_name" gorm:"primaryKey"`
 	Email        string `json:"email"`
 	Password     string `json:"password" binding:"min=6,max=24,required"`
 	PhoneNumber  string `json:"phone_number"`
